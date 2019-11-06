620dcb6987dfe26faab10742e3c34667129d18a8
Nethelbert <gilblasse@gmail.com>
2019-11-06 15:32:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,6 @@ require 'pry'
 def key_for_min_value(name_hash)
   smallest_num = name_hash.first[1]
     name_hash.each do |k,v| 
-      binding.pry
         if v < smallest_num
            smallest_num = v
         end
