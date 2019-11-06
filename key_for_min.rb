1d6cc2f7487619e2baa7a69eabdb6f96ca6bf568
Nethelbert <gilblasse@gmail.com>
2019-11-06 15:26:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
-  smallest_num = name_hash[]
+  smallest_num = name_hash.first[1]
     name_hash.each do |k,v| 
       if v < smallest_num
          smallest_num = v
